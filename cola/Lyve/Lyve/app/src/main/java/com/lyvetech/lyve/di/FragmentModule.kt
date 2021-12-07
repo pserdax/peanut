3551c5bb453260312d4b9f9b1d5a211011d6e5f6
pserdax <parahatgeldiyew.s@gmail.com>
2021-12-07 23:12:27 +0200

    Saving run to database once user clicks on finish run btn

---
+++ cola/Lyve/Lyve/app/src/main/java/com/lyvetech/lyve/di/FragmentModule.kt
@@ -0,0 +1,10 @@
+package com.lyvetech.lyve.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+
+@Module
+@InstallIn(FragmentComponent::class)
+object FragmentModule {
+}
