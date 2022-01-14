11a69d52837ff0a2859e2c4042e121588e85bd6f
pserdax <parahatgeldiyew.s@gmail.com>
2022-01-14 23:10:05 +0200

    Added bottom sheet and some minor changes

--- app/src/main/java/com/lyvetech/peanut/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/lyvetech/peanut/ui/fragments/MainFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.lyvetech.peanut.R
 import com.lyvetech.peanut.databinding.FragmentFirstBinding
 
 /**
@@ -14,9 +17,19 @@ class MainFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
+    private lateinit var bottomSheetDialog: BottomSheetDialog
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Bottom sheet layout components
+        bottomSheetDialog = context?.let { BottomSheetDialog(it) }!!
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +43,11 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAdd.setOnClickListener {
+            bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetDialog.show()
+        }
     }
 
     override fun onDestroyView() {
